0900305e8795199128a047bd4135da14fbd68fe3
FrancisPal <92956202+FrancisPalustre@users.noreply.github.com>
2023-12-14 14:57:38 -0800

    Update main.cpp

--- C++98/BFS_98/main.cpp
+++ C++98/BFS_98/main.cpp
@@ -176,7 +176,7 @@ map<string, vector<string> > removeEdges(map<string, vector<string> > graph, vec
 }
 
 //Print the path and movements for a robot to move from start to desired node
-void roboMove(map<string, vector<string> > newGraph, string startNode, string desiredNode) {
+void path(map<string, vector<string> > newGraph, string startNode, string desiredNode) {
     vector<string> carPath = BFS(newGraph, startNode, desiredNode);
 
     //couts the final path
@@ -216,7 +216,7 @@ int main(int argc, char **argv) {
     //Resultant graph after changes
     graph = removeEdges(graph, removeVec);
 
-    roboMove(graph, startingNode, desiredNode);
+    path(graph, startingNode, desiredNode);
 
     return 0;
 }
